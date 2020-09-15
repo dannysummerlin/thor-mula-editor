0b4e7875ba5d53cb3f956e0713b3116e2bbce942
Danny Summerlin <danny@summerlin.co>
2020-09-14 23:22:15 -0400

    point of insertion works, now to work on naming

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,16 @@
+[
+	"styles/prismeditor.min.css",
+	"styles/prism-coy.css",
+	"styles/prism-okaidia.css",
+	"styles/main.css"
+].forEach((s)=>{
+	var elem = document.createElement('link')
+	elem.rel = 'stylesheet'
+	elem.setAttribute('href', chrome.extension.getURL(s))
+	elem.setAttribute('title', 'thor')
+	document.body.appendChild(elem)
+})
+
 swapEditor = ()=>{
 	var formulaEditorOuter = document.getElementsByClassName('formulaEditorOuter')
 	var insertFieldButton = document.querySelector('input[name=insertField]') // don't replace the advanced editor
@@ -9,7 +22,6 @@ swapEditor = ()=>{
 				.map((e)=>{return {label: "Field: " + e.text, value: e.value}})
 			fields.shift()
 		}
-//TODO make auto fill box follow cursor better
 		formulaEditorOuter = formulaEditorOuter[0]
 		let formulaEditor = formulaEditorOuter.getElementsByTagName('textarea')[0]
 		const content = formulaEditor.value
@@ -18,31 +30,78 @@ swapEditor = ()=>{
 		var app = new Vue({
 			template: `
 			<div id="app" :class="theme">
-				<Mentionable :keys="triggerKeys" :items="functionValues" placement="auto" omit-key>
-					<prism-editor class="height-400" v-model="content" :highlight="highlighter" @input="updateRealValue" line-numbers></prism-editor>
+				<Mentionable ref="suggestions" :keys="triggerKeys" @caret="updateCaretPosition" :items="functionValues" placement="right-start" omit-key>
+					<prism-editor id="thorEditor" ref="editor" class="height-400" v-model="content" :highlight="highlighter" @input="updateRealValue" line-numbers></prism-editor>
 				</Mentionable>
-				<select v-model="theme" class="themeSelector">
-					<option v-for="t in themes" v-bind:value="t.value">
-						{{ t.label }}
-					</option>
-				</select>
+				<div class="controls" style="font-size:14px">
+					<select v-model="theme" class="themeSelector">
+						<option v-for="t in themes" v-bind:value="t.value">
+							{{ t.label }}
+						</option>
+					</select>
+					<label for="fontSizeSelector" style="color:black">Font size:</label>
+					<input type="range" @change="updateFontSize" min="12" max="18" id="fontSizeSelector" name="fontSizeSelector" v-model="fontSize" class="slider"/><i style="color:black">{{ fontSize }}</i>
+				</div>
 			</div>`,
 			data: {
 				content: content,
+				fontSize: 14,
 				functionValues: functionValues.concat(fields),
 				themes: [
 					{value: 'okaidia', label: 'Dark Mode'},
 					{value: 'coy', label: 'Light Mode'}
 				],
 				theme: 'okaidia',
+				styleRef: false,
 				triggerKeys: ['?']
 			},
 			methods: {
 				highlighter(code) {
 					return Prism.highlight(code, Prism.languages.js, "excel-formula")
 				},
+				updateCaretPosition(o) {
+					const p = this.getCursorXY(o.input,o.selectionEnd)
+					if(!this.styleRef) {
+						let s = Object.values(document.styleSheets).filter((c)=>{ return c.title == "thor" && c.href?.includes('main.css') })[0]
+						if(s.rules)
+							this.styleRef = s.rules[0]
+					}
+					const e = document.getElementById('app').getBoundingClientRect()
+					this.styleRef.style.setProperty('--caretPositionX', (e.x + p.x +  4 + this.fontSize) + "px")
+					this.styleRef.style.setProperty('--caretPositionY', (e.y + p.y + 14 + this.fontSize) + "px")
+				},
+				getCursorXY(input, selectionPoint) {
+					const {
+						offsetLeft: inputX,
+						offsetTop: inputY,
+					} = input
+					const div = document.createElement('div')
+					const copyStyle = getComputedStyle(input)
+					for (const prop of copyStyle) {
+						div.style[prop] = copyStyle[prop]
+					}
+					const swap = '.'
+					const inputValue = input.tagName === 'INPUT' ? input.value.replace(/ /g, swap) : input.value
+					const textContent = inputValue.substr(0, selectionPoint)
+					div.textContent = textContent
+					if (input.tagName === 'TEXTAREA') div.style.height = 'auto'
+					if (input.tagName === 'INPUT') div.style.width = 'auto'
+					const span = document.createElement('span')
+					span.textContent = inputValue.substr(selectionPoint) || '.'
+					div.appendChild(span)
+					document.body.appendChild(div)
+					const { offsetLeft: spanX, offsetTop: spanY } = span
+					document.body.removeChild(div)
+					return {
+						x: inputX + spanX,
+						y: inputY + spanY,
+					}
+				},
 				updateRealValue(i,s) {
 					formulaEditor.value = this.content
+				},
+				updateFontSize() {
+					document.documentElement.style.setProperty('--fontSize', this.fontSize + "px")
 				}
 			}
 		})
